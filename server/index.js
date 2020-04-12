89abab420ce0e3db9cbd39725a215ad3b7ad586c
iskandar <iskandar@qubit.asia>
2020-04-12 15:33:54 +0800

    add deps

--- server/index.js
+++ server/index.js
@@ -1,2 +1,48 @@
+const express = require('express');
+const app = express();
+const formidable = require('formidable');
+const mongo = require('mongodb');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+const router = express.Router();
 
+var cors = require('cors');
 
+app.use(express.static(__dirname + '/public'));
+app.use(cors());
+
+function onConnection(socket) {
+    console.log("Socket.id : " + socket.id);
+
+    socket.on('k-master', (data) => {
+        socket.broadcast.emit('k-master', data);
+    });
+
+    socket.on('k-client', (data) => {
+        socket.broadcast.emit('k-client', data);
+    });
+
+    socket.on('disconnect', function() {
+        const item = { type: 'Disconnect', id: socket.id };
+        console.log("Disconnected", item);
+    });
+}
+
+io.on('connection', onConnection);
+
+router.get('/k-master', function() {
+    app.use(express.static(__dirname + '/master'));
+    app.use(cors());
+});
+
+router.get('/k-client', function() {
+    app.use(express.static(__dirname + '/client'));
+    app.use(cors());
+});
+
+app.use('/api', router);
+
+http.listen(port, () => {
+    console.log('listening on port ' + port)
+});
---
+++ server/monggoconnect.js
@@ -0,0 +1,30 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/zankarok";
+
+activate();
+
+function activate() {
+    startconnection();
+}
+
+function startconnection() {
+    const options = {
+        keepAlive: 1,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    };
+
+    MongoClient.connect(url, options)
+        .then((db) => {
+            console.log('DB created/connected');
+            console.log('Connection Info :', db);
+            // db.close();
+        })
+        .catch(err => {
+            console.log('DB Connection Error :' + err.message);
+            setTimeout(function() {
+                console.log('Reconnect DB');
+                startconnection();
+            }, 3000)
+        });
+}
